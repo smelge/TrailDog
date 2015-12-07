88d5daa45c08abc4c788a358c27e38221cbe1988
smelge <traildogfilms@gmail.com>
2015-12-07 23:33:37 +0000

    Update database_connect.php

--- includes/database_connect.php
+++ includes/database_connect.php
@@ -1,8 +1,8 @@
 <?php
-	$hostname = "localhost";
-	$username = "traildog_query";
-	$password = "^uRx+TonLfdG";
-	$database = "traildog";
+	$hostname = "";
+	$username = "";
+	$password = "";
+	$database = "";
 
 	$dblink = mysqli_connect($hostname, $username, $password);
 	mysqli_select_db($dblink, "traildog_traildata") or die ("Could not connect to TrailData");

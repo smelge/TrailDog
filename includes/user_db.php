5e9c36e83774c93e3ba7f623785293ff6a97a08c
smelge <traildogfilms@gmail.com>
2015-12-07 23:34:07 +0000

    Update user_db.php

--- includes/user_db.php
+++ includes/user_db.php
@@ -1,8 +1,8 @@
 <?php
-	$hostname = "localhost";
-	$username = "traildog_smf1";
-	$password = "V@SstlhmxF60]@1";
-	$database = "traildog_smf1";
+	$hostname = "";
+	$username = "";
+	$password = "";
+	$database = "";
 
 	$user_db = mysqli_connect($hostname, $username, $password);
 	mysqli_select_db($user_db, $database) or die ("Could not connect to User Database");
